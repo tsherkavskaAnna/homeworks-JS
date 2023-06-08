5c96cade9d53ed0d3d27d9902fae7cfb09ae96d0
annatsherkavska <katotoka1985@gmail.com>
2023-06-08 12:55:36 +0200

    add task names

---
+++ script6.js
@@ -0,0 +1,66 @@
+/*Dato un array contenente nomi di persone, identificare il nome che è utilizza più lettere. Nel conteggio non considerare gli spazi e il -
+
+Stampare su console il nome identificato, unitamente al numero di lettere che utilizza. In caso di più nomi con lo stesso numero di lettere utilizzate, stampare quello che appare prima nell'array (indice minore)
+
+Utilizzare il file js allegato, nel quale è già presente un array con 40 nominativi.*/
+const names = [
+    'Antonia Maglio',
+    'Veronica Carosone',
+    'Ilaria Garobbio',
+    'Lucia Sbarbaro',
+    'Rodolfo Telesio',
+    'Virginia Verri',
+    'Gabriele Foa',
+    'Gabriella Chindamo',
+    'Arnulfo Pertini',
+    'Pompeo Granatelli',
+    'Pasqual Abatantuono',
+    'Vito Curatoli',
+    'Lidia Bodoni',
+    'Raffaella Ritacca',
+    'Morena Pertini',
+    'Armando Ariasso',
+    'Paulina Ferrata',
+    'Beatrice Bernetti',
+    'Elmo Crespi',
+    'Liliana Bignardi',
+    'Michelangelo Carocci',
+    'Elena Luciani',
+    'Rodolfo Grossi-Armani',
+    'Gianmarco Ginesio',
+    'Salvo Treccani',
+    'Nino Padovano',
+    'Marissa Caccianemico',
+    'Martina Zacchia',
+    'Ennio Cossiga',
+    'Flavia Iacovelli-Viola',
+    'Rocco Iannucci-Palumbo',
+    'Bruno Comisso',
+    'Milo Ariosto-Gentili',
+    'Marissa Dibiasi',
+    'Carla Bignardi',
+    'Ottone Natta',
+    'Martino Gremese',
+    'Costanzo Comboni',
+    'Manuel Contarini-Piazzi',
+    'Licia Roth'];
+
+const nome = document.querySelector('body');
+
+let longestName = '';
+let lenghtOfName = 0;
+
+for (let i = 0; i < names.length; i++) {
+    const currentName = names[i].replace(/[\s-]/g, '');
+    const length = currentName.length;
+
+    if(length > lenghtOfName) {
+        longestName = names[i];
+        lenghtOfName = length
+    }
+}
+
+ nome.innerHTML = "Il nome piu lungo è: " + longestName + "<br>" + "con le lettere: " + lenghtOfName;
+ 
+
+
